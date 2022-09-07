89e3fbe9482c14934371dfb769b046b5e48524b8
DawsonXBancroft <60516345+DawsonXBancroft@users.noreply.github.com>
2022-09-06 18:12:38 -0700

    Adding yaml parsing
    Incomplete addition of yaml parsing

--- windows/decodeSDData.py
+++ windows/decodeSDData.py
@@ -1,7 +1,9 @@
 # @author: DawsonXBancroft
-import string
+# example use:
+# python decodeSDData.py -type DEBUG -inputfile .\ExampleInput\210708_FLIGHT_DATAONLY_LORA_ONLY.txt
 import sys
 import os.path
+import yaml
 from enum import Enum
 
 
@@ -17,6 +19,8 @@ input_file_path = "null"
 config_file_path = "null"
 output_file_path = "null"
 gui = False
+usage = "Usage: main.py [-inputfile <PATH> -type <{DEBUG, NO_ATTACH, NEMO}> [-outputfile <PATH>] [-verbosity <{LOW, " \
+        "MEDIUM, HIGH, DEBUG}>]] || [-gui]] "
 
 
 def parseCommandLineArgs():
@@ -26,9 +30,9 @@ def parseCommandLineArgs():
     global config_file_path
     global output_file_path
     global gui
+    global usage
     if len(args) == 0:
-        print("Usage: main.py [-inputfile <PATH> -type <{DEBUG, NO_ATTACH, NEMO}> [-outputfile <PATH>] [-verbosity"
-              " <{LOW, MEDIUM, HIGH, DEBUG}>]] || [-gui]]")
+        print(usage)
         sys.exit()
     for i in range(0, len(args), 1):
         if args[i] == "-verbosity":
@@ -60,7 +64,14 @@ def parseCommandLineArgs():
             if os.path.exists(args[i + 1]):
                 usr_input = input("File " + args[i + 1] + " exists. Would you like to overwrite it? (y/n)")
                 if usr_input.lower() == "y":
-
+                    output_file_path = args[i + 1]
+                    i = i + 1
+                else:
+                    print("Okay; Exiting program")
+                    sys.exit()
+            else:
+                output_file_path = args[i + 1]
+                i = i + 1
         elif args[i] == "-type":
             if args[i + 1].upper() == "DEBUG":
                 config_file_path = "config/debug.yaml"
@@ -70,6 +81,7 @@ def parseCommandLineArgs():
                 sys.exit()
         elif args[i] == "-gui":
             gui = True
+
     if verbosity.value > Verbosity.HIGH.value:
         print("END OF PARSING ARGUMENTS")
         print("    verbosity     : " + verbosity.name)
@@ -79,14 +91,40 @@ def parseCommandLineArgs():
         print("    type          : " + config_file_path)
         print()
 
+    if not gui:
+        if input_file_path == "null":
+            print("FATAL: INPUT FILE NOT SPECIFIED")
+            print(usage)
+            sys.exit()
+        if config_file_path == "null":
+            print("FATAL: TYPE NOT SPECIFIED")
+            print(usage)
+            sys.exit()
+
+
+def configRead():
+    global config_file_path
+
+    # get column labels (column headers for csv file)
+    for key, value in yaml.safe_load(open(config_file_path))['COL_LABELS'].items():
+        print(key, value)
+
+    # get column vars (variables that will go in each row of the csv file)
+    for key, value in yaml.safe_load(open(config_file_path))['COL_VARS'].items():
+        print(key, value)
+
+    # get sd card setup
+    for key, value in yaml.safe_load(open(config_file_path))['SDCARD_SETUP'].items():
+        print(key, value)
+
+
 
 def __main__():
     # PARSE COMMAND LINE ARGUMENTS
     parseCommandLineArgs()
 
-    # SET INPUT FILE, OUTPUT FILE, AND CONFIG INPUT FILE
-
-    # READ IN YAML FILE
+    # READ IN YAML (config) FILE
+    configRead()
 
     # IF CONFIG DATA READ THAT IN (MAY CONTAIN # OF DATA POINTS)
 
@@ -94,3 +132,5 @@ def __main__():
 
 
 __main__()
+
+
