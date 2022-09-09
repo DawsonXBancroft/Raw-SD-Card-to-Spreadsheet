56f5ab0a44cf6399d6807285d586b2b62074969c
Dawson Bancroft <dawsonxbancroft@gmail.com>
2022-09-08 20:24:45 -0700

    Added debug functionality to yaml_config.py

--- classes/yaml_config.py
+++ classes/yaml_config.py
@@ -1,4 +1,5 @@
 import yaml
+from verbosity import *
 class YamlConfig():
 
     # Class for holding configuration of SD Card and CSV file
@@ -9,20 +10,34 @@ class YamlConfig():
 
 
     # When constructed, the config_file_path is required
-    def __init__(self, config_file_path):
+    def __init__(self, config_file_path, verbosity):
         self.config_file_path = config_file_path
+        self.verbosity = verbosity
 
     def readInConfig(self):
         # get column labels (column headers for csv file)
+        if self.verbosity.value > Verbosity.HIGH.value:
+            print("\n    Getting Column Labels")
         for key, value in yaml.safe_load(open(self.config_file_path))['COL_LABELS'].items():
-            col_labels_dict[key] = value
+            self.col_labels_dict[key] = value
+            if self.verbosity.value > Verbosity.HIGH.value:
+                print(key, value)
+
 
         # get column vars (variables that will go in each row of the csv file)
+        if self.verbosity.value > Verbosity.HIGH.value:
+            print("\n    Getting Column Variables")
         for key, value in yaml.safe_load(open(self.config_file_path))['COL_VARS'].items():
-            col_vars_dict[key] = value
+            self.col_vars_dict[key] = value
+            if self.verbosity.value > Verbosity.HIGH.value:
+                print(key, value)
 
         # get sd card setup
+        if self.verbosity.value > Verbosity.HIGH.value:
+            print("\n    Getting Column SD Card Setup")
         for key, value in yaml.safe_load(open(self.config_file_path))['SDCARD_SETUP'].items():
-            sd_card_setup_dict[key] = value
+            self.sd_card_setup_dict[key] = value
+            if self.verbosity.value > Verbosity.HIGH.value:
+                print(key, value)
 
 
--- decodeSDData.py
+++ decodeSDData.py
@@ -1,6 +1,6 @@
 # @author: DawsonXBancroft
 # example use:
-# python decodeSDData.py -type DEBUG -inputfile .\ExampleInput\210708_FLIGHT_DATAONLY_LORA_ONLY.txt
+# python decodeSDData.py -type DEBUG -inputfile .\ExampleInput\210708_FLIGHT_DATAONLY_LORA_ONLY.txt -verbosity DEBUG
 import sys
 sys.path.append('classes/')
 # User defined classes
@@ -11,11 +11,11 @@ from cmdline_args_parse import *
 
 def __main__():
     # PARSE COMMAND LINE ARGUMENTS
-    sd_cfg = CMDLineParser()
-    sd_cfg.parse()
+    cmd_cfg = CMDLineParser()
+    cmd_cfg.parse()
 
     # READ IN YAML (config) FILE
-    yaml_cfg = YamlConfig(sd_cfg.config_file_path)
+    yaml_cfg = YamlConfig(cmd_cfg.config_file_path, cmd_cfg.verbosity)
     yaml_cfg.readInConfig()
 
     # IF CONFIG DATA READ THAT IN (MAY CONTAIN # OF DATA POINTS)
